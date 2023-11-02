148f206e0d44893c83ca6c437dd8413e7ed9bba6
amethystlyre <91407728+amethystlyre@users.noreply.github.com>
2023-11-02 19:52:08 +1100

    update

--- server.js
+++ server.js
@@ -4,4 +4,6 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.get('/', (req, res) => res.send('Happy Halloween!'));
+
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
